017faaa224b88b8dab8eef385f9589f9712b3a37
Davidsx <davidlau900000@gmail.com>
2024-11-22 15:26:22 +0800

    fix build error

--- src/app/useWindowSize.ts
+++ src/app/useWindowSize.ts
@@ -1,15 +1,19 @@
-import React from 'react';
+"use client";
+
+import { useLayoutEffect, useState } from "react";
 
 export default function useWindowSize(): { [T: string]: number } {
-  const getSize = (): { [T: string]: number } => ({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-  const [windowSize, setWindowSize] = React.useState(getSize);
-  React.useLayoutEffect(() => {
+  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
+  useLayoutEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const getSize = (): { width: number; height: number } => ({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
     const onResize = () => setWindowSize(getSize);
-    window.addEventListener('resize', onResize);
-    return (): void => window.removeEventListener('resize', onResize);
+    window.addEventListener("resize", onResize);
+    return (): void => window.removeEventListener("resize", onResize);
   }, []);
   return windowSize;
 }
