ef0fbbca6a913f50ac3f1d00b024134e1bc6b026
Davidsx <davidlau900000@gmail.com>
2024-11-22 16:01:08 +0800

    taller

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="relative h-screen w-screen bg-[#F0F0F0]">
       <div className="w-full h-full">
-        <div className="relative w-[250px] h-[200px]" ref={elementRef}>
+        <div className="relative w-[250px] h-[280px]" ref={elementRef}>
           <Image src="/lego.png" alt="lego" fill />
         </div>
       </div>
